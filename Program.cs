383ca08e91bcfbbaa19b9cd0f33db8f62c717e9f
Dungeoner <Dungeoner@DESKTOP-4SNPBLN>
2020-01-29 20:45:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Normal = "This is string";
+            string[] words = Normal.Split(new char[] { ' ' });
+            string Mirrored = null;
+            foreach (string s in words)
+            {
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    Mirrored = Mirrored + s[i];
+
+                }
+                Mirrored = Mirrored + " ";
+            }
+            Console.WriteLine(Mirrored);
+            Console.ReadKey();
+        }
+    }
+}
